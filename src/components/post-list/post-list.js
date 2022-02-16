b5b0032ed96b296abacad014352e7f4ab4be62a4
Ruslan Romah <romahruslan@gmail.com>
2022-02-16 18:12:09 +0200

    add counter

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,14 +2,17 @@ import React from "react";
 
 import PostListItem from '../post-list-item';
 
-const PostList = ({ posts, onDelete }) => {
+const PostList = ({ posts, onDelete, onToggleImportant, onToggleLike }) => {
     
     const elements = posts.map((item) => {
         const { id, ...itemProps } = item;
         return (
             <li key={id} className="list-group-item">
-                <PostListItem {...itemProps}
-                    onDelete={() => onDelete(id)}/>
+                <PostListItem
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLike={() => onToggleLike(id)}/>
             </li>
         )
     });
